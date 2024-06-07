fa86ea08e0a1346a49e290df2380a5078874809f
PeterPark07 <128113413+PeterPark07@users.noreply.github.com>
2024-06-07 12:46:13 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, render_template, jsonify
+import os
+from download_music import download_music
+
+app = Flask(__name__)
+
+# Directory to save downloaded music files
+MUSIC_DIR = 'static/music'
+
+# Ensure the music directory exists
+if not os.path.exists(MUSIC_DIR):
+    os.makedirs(MUSIC_DIR)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/request_music', methods=['POST'])
+def request_music():
+    query = request.form['query']
+    filename = download_music(query, MUSIC_DIR)
+    if filename:
+        return jsonify({'url': f'/static/music/{filename}'})
+    else:
+        return jsonify({'error': 'Unable to download music'}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
