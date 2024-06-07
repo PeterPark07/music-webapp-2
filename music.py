84380a458c93f827b3fc87a8147e6f14c37f51a8
PeterPark07 <128113413+PeterPark07@users.noreply.github.com>
2024-06-07 12:49:20 +0530

    Update music.py

--- music.py
+++ music.py
@@ -1,5 +1,6 @@
 from youtubesearchpython import VideosSearch
 import yt_dlp
+import os
 
 def search_youtube(query, max_results=1):
     videos_search = VideosSearch(query, limit=max_results)
@@ -7,14 +8,17 @@ def search_youtube(query, max_results=1):
     video_urls = [result['link'] for result in results['result']]
     return video_urls
 
-def download(query):
-
+def download_music(query, download_dir):
     url = search_youtube(query, max_results=1)[0]
     ydl_opts = {
         'format': 'bestaudio/best',
-        'outtmpl': '%(title)s.%(ext)s',
+        'outtmpl': os.path.join(download_dir, '%(title)s.%(ext)s'),
         'noplaylist': True,
     }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([url])
+        info_dict = ydl.extract_info(url, download=True)
+        filename = ydl.prepare_filename(info_dict)
+
+    return filename
+
