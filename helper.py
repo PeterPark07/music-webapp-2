faa3d0b7a9272ed5873beefbddbb1d54ce4d87dd
PeterPark07 <128113413+PeterPark07@users.noreply.github.com>
2024-12-10 10:07:56 +0530

    Update and rename music.py to helper.py

---
+++ helper.py
@@ -0,0 +1,23 @@
+from youtubesearchpython import VideosSearch
+import os
+import pytz
+from datetime import datetime
+import tempfile
+
+def search_youtube(query, max_results=1):
+    videos_search = VideosSearch(query, limit=max_results)
+    results = videos_search.result()
+    video_urls = [result['link'] for result in results['result']]
+    titles = [result['title'] for result in results['result']]
+    return video_urls, titles
+
+def get_current_time():
+    return datetime.now(pytz.timezone("Asia/Kolkata")).strftime("%Y-%m-%d %H:%M:%S")
+
+def save_cookie_to_file():
+    raw_cookie_value = os.getenv('cookies')    
+    temp_cookie_file = tempfile.NamedTemporaryFile(delete=False)
+    temp_cookie_file.write(raw_cookie_value.encode())
+    temp_cookie_file.close()
+    return temp_cookie_file.name
+
