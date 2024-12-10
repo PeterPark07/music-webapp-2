d96051db16a7a939cba430a4255325f2b36bd5b1
PeterPark07 <128113413+PeterPark07@users.noreply.github.com>
2024-12-10 10:18:06 +0530

    Update helper.py

--- helper.py
+++ helper.py
@@ -1,15 +1,9 @@
 from youtubesearchpython import VideosSearch
-import os
 import pytz
 from datetime import datetime
 import tempfile
+import os
 
-def search_youtube(query, max_results=1):
-    videos_search = VideosSearch(query, limit=max_results)
-    results = videos_search.result()
-    video_urls = [result['link'] for result in results['result']]
-    titles = [result['title'] for result in results['result']]
-    return video_urls, titles
 
 def get_current_time():
     return datetime.now(pytz.timezone("Asia/Kolkata")).strftime("%Y-%m-%d %H:%M:%S")
@@ -21,3 +15,9 @@ def save_cookie_to_file():
     temp_cookie_file.close()
     return temp_cookie_file.name
 
+def search_youtube(query, max_results=1):
+    videos_search = VideosSearch(query, limit=max_results)
+    results = videos_search.result()
+    video_urls = [result['link'] for result in results['result']]
+    titles = [result['title'] for result in results['result']]
+    return video_urls, titles
